a3687c1a8bb27a37e46b3f31bc87e343254140cb
Afaf997 <afizaya076@gmail.com>
2023-11-27 16:15:00 +0530

    remove comment

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -4,8 +4,6 @@ import 'package:todo_with_getx/app/modules/home/controllers/modecontroller.dart'
 import 'package:todo_with_getx/app/modules/home/controllers/todocontroller.dart';
 import 'package:todo_with_getx/app/modules/notes/controllers/notescontroller.dart';
 
-// import '../controllers/Notes_controller.dart';
-
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
